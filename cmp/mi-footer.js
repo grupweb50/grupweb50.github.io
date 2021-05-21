3ed1bd43b41431ed733d36a5342baa0ab49ddd6b
grupweb50 <grupweb50@gmail.com>
2021-05-21 16:46:12 -0500

    Cambio

--- cmp/mi-footer.js
+++ cmp/mi-footer.js
@@ -1,7 +1,7 @@
 class MiFooter extends HTMLElement {
   connectedCallback() {
     this.innerHTML = /* html */
-      `Copyright &copy; 2021 Ricardo Armando Machorro Reyes.`;
+      `Copyright &copy; 2021 Ricardo Armando Machorro Reyes. (3CV50)`;
   }
 }
 customElements.define("mi-footer", MiFooter);
