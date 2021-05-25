42ea380db6fb64629febf6c77bd25f77263d2c66
grupweb50 <84601584+grupweb50@users.noreply.github.com>
2021-05-25 15:34:28 -0500

    Delete cmp directory

--- cmp/mi-footer.js
+++
@@ -1,7 +0,0 @@
-class MiFooter extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = /* html */
-      `Copyright &copy; 2021 Ricardo Armando Machorro Reyes. (3CV50)`;
-  }
-}
-customElements.define("mi-footer", MiFooter);
