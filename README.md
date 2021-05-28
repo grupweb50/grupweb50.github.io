bbcf1462c79bfa14b77748db82f6a8ee37416a97
grupweb50 <grupweb50a@gmail.com>
2021-05-28 16:46:51 -0500

    Cambio

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-# grupweb50.github.io
-Ejemplo De Un Sitio
-https://grupweb50.github.io/
+# rickchat
+Ejemplo de un chat y acceso a Firebase (Firestore con Google)
---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyCpeEGXQH4SZ1ndphCi5Ux7vy7ouQrkUso",
+  authDomain: "chatweb3cv50.firebaseapp.com",
+  projectId: "chatweb3cv50",
+  storageBucket: "chatweb3cv50.appspot.com",
+  messagingSenderId: "700570770554",
+  appId: "1:700570770554:web:830ddaab698f3b64e66fc3",
+  measurementId: "G-5PLSBNH0E5"
+});
